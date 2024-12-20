a2c99fa0a1eda8e2c69c20bfc42d1efda2f441ca
ferreo <harderthanfire@gmail.com>
2024-12-20 14:03:35 +0000

    Optimize and align all 3 benchmarks

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,24 +1,36 @@
 fn sieve_of_eratosthenes(n: usize) -> usize {
-    let mut is_prime = vec![true; n + 1];
-    let mut total = 0;
-    is_prime[0] = false;
-    is_prime[1] = false;
+    if n < 2 { return 0; }
+    if n == 2 { return 1; }
 
-    for i in 2..=(n as f64).sqrt() as usize {
-        if is_prime[i] {
-            for j in ((i * i)..=n).step_by(i) {
-                is_prime[j] = false;
+    let odd_count = (n - 1) >> 1;
+    let bytes = (odd_count + 7) >> 3;
+    let mut bits = vec![0xFF_u8; bytes];
+
+    let mut i = 0;
+    while 2 * i + 3 <= n {
+        if (bits[i >> 3] & (1 << (i & 7))) != 0 {
+            let prime = 2 * i + 3;
+            if prime * prime <= n {
+                let mut j = (prime * prime - 3) >> 1;
+                while j < odd_count {
+                    bits[j >> 3] &= !(1 << (j & 7));
+                    j += prime;
+                }
             }
         }
+        i += 1;
     }
 
-    for i in 2..=n {
-        if is_prime[i] {
-            total += 1;
+    let mut count = 1;
+    let mut pos = 0;
+    while pos < odd_count {
+        if (bits[pos >> 3] & (1 << (pos & 7))) != 0 {
+            count += 1;
         }
+        pos += 1;
     }
 
-    total
+    count
 }
 
 fn main() {
@@ -26,5 +38,5 @@ fn main() {
     let n = args[1].parse::<usize>().unwrap();
 
     let primes = sieve_of_eratosthenes(n);
-    println!("Primes up to {} are: {}", n, primes);
+    println!("There are {} primes between 2 and {}", primes, n);
 }
